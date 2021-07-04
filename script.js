f2be638e23e255c7d22f5c3356c11b38ea135339
Neel Adwani <nprosofficial@gmail.com>
2021-07-04 05:26:25 +0000

    Keeping the Background static

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function setup() {
 	createCanvas(500, 400);
+  background(225);
 }
 
 var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
 
 function draw() {
-	background(225);
-	
 	fill(0);
 	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
 	
