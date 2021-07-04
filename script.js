919734a1b0109f35ddb2d73c002f5a8ba70df0ca
Neel Adwani <nprosofficial@gmail.com>
2021-07-04 07:20:14 +0000

    Omitted some JS

--- script.js
+++ script.js
@@ -1,55 +1,22 @@
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
 function setup() {
 	createCanvas(1520, 680);
   background(225);
+  WebSocket
 }
 
 var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
 
 function draw() {
+  strokeWeight(30)
 	fill(0, 0, 0);
   if(mouseIsPressed){
 	  line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
 
-function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
-}
-
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
-}
